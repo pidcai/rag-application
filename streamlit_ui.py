182c7c18789f99075c2141e92e53ed18295f288f
divijesh <divijesh@divijeshs-Mac-mini.local>
2025-10-29 18:34:15 +0530

    Add GitHub Actions deploy pipeline11 divi

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -24,7 +24,7 @@ retriever = (
 
 if __name__ == "__main__":
 
-    st.header("Pi Divi Chatbot")
+    st.header("Pi Divi New Chatbot")
 
     # [DONT CHANGE] check if the messages list is empty. If empty, initialize the messages list
     if "messages" not in st.session_state:
