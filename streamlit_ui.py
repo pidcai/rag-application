a460e56cfc3e13700340b594c3542dada5facb5e
pidcai <ravi.potturi@pidatacenters.com>
2025-10-31 11:53:18 +0530

    test UI

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -24,7 +24,7 @@ retriever = (
 
 if __name__ == "__main__":
 
-    st.header("Pi V2 Chatbot")
+    st.header("Pi V3 Chatbot")
 
     # [DONT CHANGE] check if the messages list is empty. If empty, initialize the messages list
     if "messages" not in st.session_state:
