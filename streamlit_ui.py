b921960e3cb0981028fa3860e19a1ac4ae79ca80
Divijeshvarma.a <divijeshvarma.a@pidatacenters.com>
2025-11-18 13:56:29 +0530

    harness4

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -24,7 +24,7 @@ retriever = (
 
 if __name__ == "__main__":
 
-    st.header("Chatbot v2")
+    st.header("Chatbot v3")
 
     # [DONT CHANGE] check if the messages list is empty. If empty, initialize the messages list
     if "messages" not in st.session_state:
